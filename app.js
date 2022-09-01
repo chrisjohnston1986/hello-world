73d67d27278c913806625bbc34726ba2dc92b083
Chris Johnston <chrisjohnston1986@yahoo.com>
2022-08-31 21:39:24 -0500

    added javascript detailing

--- app.js
+++ app.js
@@ -15,15 +15,15 @@ let usersName = prompt('Who are you?');
 let message;
 
 if (usersName == 'Kassie'){
-  message = '. I  appreciate you for all of your guidance, during 102!'
+  message = ' I  appreciate you for all of your guidance, during 102!'
 } else if (usersName == ''){
   message = "No Name!";
 } else if (usersName == 'Justin'){
-  message = ". Welcome to my 102 website!";
+  message = " Welcome to my 102 website!";
 } else {
-  message = '. Welcome to my 102 website!';
+  message = ' Welcome to my 102 website!';
 }
 
 
 document.write('Hi, ' + usersName);
-document.write(message);
+document.write("." + "       " + message);
