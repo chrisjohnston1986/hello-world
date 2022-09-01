1a88beb26185cc160e390e445ae49e625e1f20fc
Chris Johnston <chrisjohnston1986@yahoo.com>
2022-08-31 20:57:07 -0500

    added website css details

--- app.js
+++ app.js
@@ -2,5 +2,28 @@
 
 console.log('Hello World!');
 
-let x = 10;
-console.log(x);
+let myName;
+
+myName = 'Chris';
+console.log(myName);
+
+myName = 'Trace';
+console.log(myName);
+
+let usersName = prompt('Who are you?');
+
+let message;
+
+if (usersName == 'Kassie'){
+  message = '. I  appreciate you for all of your guidance, during 102!'
+} else if (usersName == ''){
+  message = "No Name!";
+} else if (usersName == 'Justin'){
+  message = ". Welcome to my 102 website!";
+} else {
+  message = '. Welcome to my 102 website!';
+}
+
+
+document.write('Hi, ' + usersName);
+document.write(message);
